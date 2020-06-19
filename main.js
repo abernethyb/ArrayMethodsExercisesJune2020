6f094215b6b6ba3c4ad333c591bfcb25c7f003ef
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-06-19 13:40:16 -0500

    used filter method

--- main.js
+++ main.js
@@ -20,3 +20,60 @@ businesses.forEach(business => {
     `
     businessSelector.innerHTML += "<hr/>"
   });
+
+  // Array to contain all the New York businesses
+const newYorkBusinesses = businesses.filter(business => {
+    let inNewYork = false
+  
+    if (business.addressStateCode === "NY") {
+        inNewYork = true
+    }
+  
+    return inNewYork
+  })
+
+  console.log("NY businesses", newYorkBusinesses)
+
+
+ // Array to contain all the Manufacturing businesses
+ const manufacturingBusinesses = businesses.filter(business => {
+    let manufacturer = false
+  
+    if (business.companyIndustry === "Manufacturing") {
+        manufacturer = true
+    }
+  
+    return manufacturer
+  })
+
+  console.log("Manufacturing Businesses", manufacturingBusinesses)
+
+  
+  const ManufacturingBusinessSelector = document.querySelector(".manufacturing__businesses")
+
+  ManufacturingBusinessSelector.innerHTML = `<h1>Manufacturers</h1>`
+  
+  manufacturingBusinesses.forEach(Mbusiness => {
+    ManufacturingBusinessSelector.innerHTML += `
+        <h2>${Mbusiness.companyName}</h2>
+        <section>
+          ${Mbusiness.addressFullStreet},
+        </section>
+        <section>
+          ${Mbusiness.addressCity}, ${Mbusiness.addressStateCode} ${Mbusiness.addressZipCode}
+        </section>
+      `
+      ManufacturingBusinessSelector.innerHTML += "<hr/>"
+    });
+
+
+
+
+
+
+
+
+
+
+
+
